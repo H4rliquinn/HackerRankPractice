24001976e533c07a9767a24eac9cb15cd31b49e8
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-15 18:25:46 -0400

    All tests passed

--- 10 Days of Javascript/Day 3/Arrays.js
+++ 10 Days of Javascript/Day 3/Arrays.js
@@ -5,7 +5,17 @@
 *   @return {Number} The second largest number in the array.
 **/
 function getSecondLargest(nums) {
-    
+    let max=-1;
+    let second=-1;
+    for (let x in nums){
+        if (nums[x]>max){
+            second=max;
+            max=nums[x];
+        } else if (nums[x]>second && nums[x]<max){
+            second=nums[x];
+        }
+    }
+    return second;
 }
 
 let ia=[2,3,6,6,5];
