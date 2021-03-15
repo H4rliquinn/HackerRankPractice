958ca0223d990d5befa6ebdc0fe086691c2d6c7d
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-15 18:16:47 -0400

    Added Get Second Largest

---
+++ 10 Days of Javascript/Day 3/Arrays.js
@@ -0,0 +1,13 @@
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    
+}
+
+let ia=[2,3,6,6,5];
+
+console.log(getSecondLargest(ia));
