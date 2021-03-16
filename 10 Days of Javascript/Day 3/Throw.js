b472e3472c4d9df78e748ba10765efc6a174eec2
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-16 14:25:09 -0400

    All tests passed

--- 10 Days of Javascript/Day 3/Throw.js
+++ 10 Days of Javascript/Day 3/Throw.js
@@ -1,3 +1,5 @@
+const { EmitFlags } = require("typescript");
+
 /*
  * Complete the isPositive function.
  * If 'a' is positive, return "YES".
@@ -5,10 +7,22 @@
  * If 'a' is negative, throw an Error with the message "Negative Error"
  */
 function isPositive(a) {
-    return "YES";
+    try{
+        if (a==0){
+            throw 'Zero Error';
+        } else if (a<0){
+            throw 'Negative Error';
+        } else {
+            return 'YES';
+        }
+    } catch (e){
+        return e;
+    }
 }
 
 let a=1;
 console.log(isPositive(a));
-// a=0;
-// a=-3;
+a=0;
+console.log(isPositive(a));
+a=-3;
+console.log(isPositive(a));
