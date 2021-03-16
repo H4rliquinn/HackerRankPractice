0bd9bd0381247a88d8d2ce28bd1a275a847ed813
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-16 14:17:07 -0400

    Added Throw

---
+++ 10 Days of Javascript/Day 3/Throw.js
@@ -0,0 +1,14 @@
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    return "YES";
+}
+
+let a=1;
+console.log(isPositive(a));
+// a=0;
+// a=-3;
