6dfdb2a17da5bf0b950e529140483363e57d53d0
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-17 16:11:02 -0400

    Added Square class Also adjusted area to add through prototype

--- 10 Days of Javascript/Day 3/Inheritance.js
+++ 10 Days of Javascript/Day 3/Inheritance.js
@@ -4,21 +4,25 @@ class Rectangle {
         this.h = h;
     }
 
-    area(){
-        return this.w*this.h;
+
+}
+/*
+*  Write code that adds an 'area' method to the Rectangle class' prototype
+*/
+Rectangle.prototype.area= function(){
+    return this.w*this.h;
+}
+class Square extends Rectangle {
+    constructor(size){
+        super(size,size);
     }
 }
-
-/*
- *  Write code that adds an 'area' method to the Rectangle class' prototype
- */
-
 /*
  * Create a Square class that inherits from Rectangle and implement its class constructor
  */
 
 const rec = new Rectangle(3, 4);
-// const sqr = new Square(3);
+const sqr = new Square(3);
 
 console.log(rec.area());
-// console.log(sqr.area());
+console.log(sqr.area());
