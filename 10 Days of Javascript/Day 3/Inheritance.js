c014c22f02ff0c75ad5359caa7db339d136d89fd
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-17 15:59:13 -0400

    Added Inheritance

---
+++ 10 Days of Javascript/Day 3/Inheritance.js
@@ -0,0 +1,22 @@
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+
+
+}
+
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+
+const rec = new Rectangle(3, 4);
+// const sqr = new Square(3);
+
+console.log(rec.area());
+// console.log(sqr.area());
