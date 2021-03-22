0cc681fece383c91d35aaa1926d268a698020bf5
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-22 17:16:44 -0400

    Throw in a little bitwise transaction

--- 10 Days of Javascript/Day 6/BitwiseOperators.js
+++ 10 Days of Javascript/Day 6/BitwiseOperators.js
@@ -1,7 +1,8 @@
 function getMaxLessThanK(n,k){
+    let max=0;
     for (let x=1;x<n;x++){
         for (y=x+1;y<n+1;y++){
-            console.log(x,y);
+            console.log(x,y,x&y);
         }
     }
 }
