cbcf6b3e9b4722d32a4811e08ff9b647c21fb172
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-22 16:36:55 -0400

    Used arrow function in map All tests passed

--- 10 Days of Javascript/Day 5/ArrowFunctions.js
+++ 10 Days of Javascript/Day 5/ArrowFunctions.js
@@ -1,6 +1,12 @@
 function modifyArray(nums) {
-
+    let newNums=nums.map(item=>{
+        if (item%2===0){
+            return item*2;
+        } else{
+            return item*3;
+        }
     })
+    return newNums;
 }
 
 let a=[1,2,3,4,5];
