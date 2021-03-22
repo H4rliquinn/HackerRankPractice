d3a7d376643950dfb547a9aae5172b3c4b1c517b
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-22 17:31:18 -0400

    Found correct X&Y, return <k Short circuits at max of k-1

--- 10 Days of Javascript/Day 6/BitwiseOperators.js
+++ 10 Days of Javascript/Day 6/BitwiseOperators.js
@@ -1,13 +1,16 @@
-function getMaxLessThanK(n,k){
-    let max=0;
-    for (let x=1;x<n;x++){
-        for (y=x+1;y<n+1;y++){
-            console.log(x,y,x&y);
+    function getMaxLessThanK(n,k){
+        let max=0;
+        for (let x=1;x<n;x++){
+            
+            for (let y=x+1;y<n+1;y++){
+                if((x&y)>max && (x&y)<k) max=(x&y);
+                if (max==k-1) return max;
+            }
         }
+        return max;
     }
-}
 
-let n=5;
-let k=2;
+let n=8;
+let k=6;
 
 console.log(getMaxLessThanK(n, k));
