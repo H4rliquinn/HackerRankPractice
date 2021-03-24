8d2816ac37f5872249f0ad55d5a5dec05d5367d5
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-24 16:23:07 -0400

    Create Date object. Return day string All tests passed

--- 10 Days of Javascript/Day 6/JavascriptDates.js
+++ 10 Days of Javascript/Day 6/JavascriptDates.js
@@ -1,6 +1,12 @@
 function getDayName(dateString) {
     let dayName;
-    // Write your code here
+    dayName=new Date(dateString);
     
-    return dayName;
+    return dayName.toLocaleDateString( 'en-US', { weekday: 'long' }); 
 }
+
+let ds="10/11/2009"
+console.log(getDayName(ds));
+
+ds="11/10/2010"
+console.log(getDayName(ds));
