c9e9d3e67b63bd11319d04fecdc425cef5a60950
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-25 13:45:10 -0400

    Not connecting the ORs correctly but partially working

--- 10 Days of Javascript/Day 7/RegularExpressionsII.js
+++ 10 Days of Javascript/Day 7/RegularExpressionsII.js
@@ -4,10 +4,23 @@ function regexVar() {
      * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
      * followed by one or more letters.
      */
-    
+    let re= new RegExp('^M(?=r)s?.+|D(?=r).+|E(?=r).+\s+[a-zA-Z]+');
     
     /*
      * Do not remove the return statement
      */
     return re;
 }
+let s='Mr. E';
+console.log(s.match(regexVar()));
+
+s='Mrs. Y';
+console.log(s.match(regexVar()));
+
+s='Dr#Joseph';
+console.log(s.match(regexVar()));
+
+s='Er .Abc';
+console.log(s.match(regexVar()));
+s='Er. Abc';
+console.log(s.match(regexVar()));
