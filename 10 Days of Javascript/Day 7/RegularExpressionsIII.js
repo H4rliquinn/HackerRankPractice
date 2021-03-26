8ee33bcb679ba4d28e47f9042bddfea0a52e9923
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-26 16:19:39 -0400

    Directions unclear. This finds integer portion. Not all numbers.

--- 10 Days of Javascript/Day 7/RegularExpressionsIII.js
+++ 10 Days of Javascript/Day 7/RegularExpressionsIII.js
@@ -3,10 +3,24 @@ function regexVar() {
      * Declare a RegExp object variable named 're'
      * It must match ALL occurrences of numbers in a string.
      */
-    
+    let re= new RegExp(/(^|\s)\d+/g); 
     
     /*
      * Do not remove the return statement
      */
     return re;
 }
+
+function main() {
+    const re = regexVar();
+    const s = "102, 1948948 and 1.34 and 4.5";
+    
+    const r = s.match(re);
+    
+    console.log(r);
+    // for (const e of r) {
+    //     console.log(e);
+    // }
+}
+
+main();
