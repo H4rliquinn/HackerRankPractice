9a92014783df8d95bab56a7f2230f6b3bd295e37
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-08 12:07:35 -0400

    Added Clear Button

--- 10 Days of Javascript/Day 9/binaryCalculator.js
+++ 10 Days of Javascript/Day 9/binaryCalculator.js
@@ -2,3 +2,8 @@ function add(a){
     let face=document.getElementById("res");
     face.innerText+=a;
 }
+
+function clearRes(){
+    let face=document.getElementById("res");
+    face.innerText='';
+};
