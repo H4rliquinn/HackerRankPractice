feafd5b9ecf3d98ffa173fe94e12c0da91194cd4
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-08 12:55:11 -0400

    Calculator works but not getting a score No error given. Probably be cause I didn't do it their way

--- 10 Days of Javascript/Day 9/binaryCalculator.js
+++ 10 Days of Javascript/Day 9/binaryCalculator.js
@@ -7,3 +7,12 @@ function clearRes(){
     let face=document.getElementById("res");
     face.innerText='';
 };
+
+function calculate(){
+    let face=document.getElementById("res");
+    let argList=face.innerText.split(/[^01]/);
+    let stuff=face.innerText.match(/\D/);
+    let answer=eval(parseInt(argList[0],2)+face.innerText.match(/\D/)+parseInt(argList[1],2));
+    answer=Math.floor(answer);
+    face.innerText=answer.toString(2);
+}
